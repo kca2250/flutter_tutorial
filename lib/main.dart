6b4ecca37dc08d693964e7b06dd8c63465bdf5c4
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2023-08-25 16:26:43 +0900

    ✨ session1完了 (#13)

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,81 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Spacer(
+                flex: 2,
+              ),
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: Placeholder(
+                  fallbackHeight: 100,
+                  fallbackWidth: 100,
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Spacer(
+                        flex: 2,
+                      ),
+                      Text(
+                        '** ℃',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '** ℃',
+                        style: TextStyle(fontSize: 18, color: Colors.red),
+                      ),
+                      Spacer(
+                        flex: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Center(
+                child: Row(
+                  children: [
+                    Spacer(
+                      flex: 2,
+                    ),
+                    Text(
+                      'Close',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Reload',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    )
+                  ],
+                ),
+              ),
+              Spacer(
+                flex: 2,
+              ),
+            ],
+          ),
         ),
       ),
     );
