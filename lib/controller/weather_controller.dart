6a0c0b46ffa8ea8a825a466c70c0b399750cae27
kca2250 <2250katok@gmail.com>
2023-09-01 11:42:39 +0900

    WeatherControllerを作成

---
+++ lib/controller/weather_controller.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: sort_constructors_first
+
+import 'package:flutter_training/model/weather.dart';
+
+class WeatherController extends Weather {
+  WeatherController(super.weatherCondition);
+}
