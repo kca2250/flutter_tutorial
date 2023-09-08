96711317d6494f2b9825274f9fc1e2c838bb7d0a
kca2250 <2250katok@gmail.com>
2023-09-08 18:45:24 +0900

    fetchThrowsWeather()を利用するように変更、例外をキャッチする機能を追加

--- lib/model/weather.dart
+++ lib/model/weather.dart
@@ -7,28 +7,14 @@ class Weather {
   // weatherConditionを定義
   final String weatherCondition;
 
-  // iconのパスを定義
-  final _sunnyIcon = 'images/sunny.svg';
-  final _cloudyIcon = 'images/cloudy.svg';
-  final _rainyIcon = 'images/rainy.svg';
-
   // yumemi_weatherパッケージを使って天気を取得する
   String fetchWeather() {
     final yumemiWeather = YumemiWeather();
-    final weather = yumemiWeather.fetchSimpleWeather();
-    return weather;
-  }
-
-  // fetchWeather()から返される値に応じて、天気アイコンパスを返す
-  String? getWeatherIcon() {
-    final weatherCondition = fetchWeather();
-    switch (weatherCondition) {
-      case 'cloudy':
-        return _cloudyIcon;
-      case 'rainy':
-        return _rainyIcon;
-      default:
-        return _sunnyIcon;
+    try {
+      final weather = yumemiWeather.fetchThrowsWeather('tokyo');
+      return weather;
+    } on YumemiWeatherError catch (e) {
+      return 'error';
     }
   }
 }
