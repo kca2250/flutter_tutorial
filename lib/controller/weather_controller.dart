2eae83cad6130abf5e5ca1153e8913a0691ed008
kca2250 <2250katok@gmail.com>
2023-09-14 17:29:16 +0900

    天気情報とエラーがあるかを返す情報

--- lib/controller/weather_controller.dart
+++ lib/controller/weather_controller.dart
@@ -1,10 +1,31 @@
-import 'package:flutter_training/model/weather.dart';
+import 'dart:convert';
+
+import 'package:flutter_training/model/weather_request/weather_request.dart';
+import 'package:flutter_training/model/weather_result/weather_result.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherController {
-  final Weather _weather = Weather(weatherCondition: '');
+  WeatherResult getWeather() {
+    try {
+      final responseJson = YumemiWeather().fetchWeather(
+        jsonEncode(
+          const WeatherRequest().toJson(),
+        ),
+      );
 
-  String getWeather() {
-    final result = _weather.fetchWeather();
-    return result;
+      final response = WeatherResult.fromJson(
+        jsonDecode(responseJson) as Map<String, dynamic>,
+      );
+
+      return response;
+    } on YumemiWeatherError catch (e) {
+      return const WeatherResult(
+        weatherCondition: '',
+        maxTemperature: 0,
+        minTemperature: 0,
+        date: '',
+        hasError: true,
+      );
+    }
   }
 }
