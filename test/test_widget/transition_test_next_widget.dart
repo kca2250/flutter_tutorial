b97483070d549944823eb31a4467059f95fb75b4
kca2250 <2250katok@gmail.com>
2023-09-21 13:39:01 +0900

    必要なmockWidgetを作成

---
+++ test/test_widget/transition_test_next_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Next Screen')),
+    );
+  }
+}
